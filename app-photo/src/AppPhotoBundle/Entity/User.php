53f415ed962903bebb715066c5c41f7ae31c7ef0
leclercweb <lavega45@yahoo.fr>
2016-12-14 16:06:34 +0100

    commit

---
+++ app-photo/src/AppPhotoBundle/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+// src/AppPhotoBundle/Entity/User.php
+
+namespace AppPhotoBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
