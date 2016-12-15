3b6e24ac0fc2120d56c3504a86d372611b3b994a
cyril-lemaire <cyril.lemaire@wanadoo.fr>
2016-12-15 10:16:03 +0100

    Debug des redirections de FOSUser à la connexion (/ renvoie vers homepage si user non loggué, la userpage sinon)

--- app-photo/src/AppPhotoBundle/Controller/DefaultController.php
+++ app-photo/src/AppPhotoBundle/Controller/DefaultController.php
@@ -3,8 +3,9 @@
 namespace AppPhotoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\UserBundle\Model\UserInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 class DefaultController extends Controller
 {
     /**
@@ -14,9 +15,11 @@ class DefaultController extends Controller
     public function indexAction()
     {
 		$user = $this->getUser();
-		if (!is_object($user) || !$user instanceof UserInterface) {
-			throw new AccessDeniedException('This user does not have access to this section.');
+		if (is_object($user) && $user instanceof UserInterface) {
+			return $this->redirectToRoute('fos_user_profile_show');
 		}
-		return $this->redirectToRoute('fos_user_profile_show');
+		return $this->render('@AppPhoto/Default/index.html.twig', array(
+			'user' => $user,
+		));
     }
 }
